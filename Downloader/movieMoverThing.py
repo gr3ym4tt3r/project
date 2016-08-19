832f3815150949c77f769d249e55c8db7ea79372
Jon <krypt3dkn1gh7@gmail.com>
2016-08-19 16:52:17 -0500

    Create movieMoverThing.py

---
+++ Downloader/movieMoverThing.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from re import search, sub
+from os import listdir, rename
+from shutil import move, rmtree
+
+source = 'D:\\CompleteDownload\\Finished\\'
+dest = 'D:\\Movies\\Moovies\\'
+
+def clean(movieName):
+    movie = search('^[^\d{3,4}p]+', movieName)
+    return sub('\.', ' ', movie.group())[:-1]
+
+if __name__ == '__main__':
+    for movies in listdir(source):
+        path = '{}{}\\'.format(source, movies)
+        for movies in listdir(path):
+            if movies.endswith('mp4'):
+                print('[+] Found: {}'.format(movies))
+                print('[+] Renaming {} to {}'.format(movies, clean(movies)))
+                rename('{}{}'.format(path, movies), '{}{}.mp4'.format(path, clean(movies)))
+                print('[+] Moving {} to {}\n'.format(clean(movies), dest))
+                move('{}\\{}.mp4'.format(path, clean(movies)), dest)
+                rmtree(path, ignore_errors=True)
