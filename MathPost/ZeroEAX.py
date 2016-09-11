e8b48a5df45b4b8c9e7878c4254837619240468e
Jon <krypt3dkn1gh7@gmail.com>
2016-09-11 01:13:39 -0500

    Create ZeroEAX.py

---
+++ MathPost/ZeroEAX.py
@@ -0,0 +1,21 @@
+from random import sample
+
+char = []
+
+def chars():
+    for x in range(0x21, 0x7f):
+        char.append('{:02x}'.format(x))
+
+def zeroEAX():
+    while True:
+        value = int(''.join(sample(char, 4)), 16)
+        value2 = int(''.join(sample(char, 4)), 16)
+        if value & value2 == 0:
+            return value, value2
+
+chars()
+print('[+] Quick way to zero out EAX')
+while True:
+    for values in zeroEAX():
+        print('\t[+] AND EAX, {:08x}'.format(values))
+    break
