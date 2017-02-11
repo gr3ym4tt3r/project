af30bd2d92a37d074af8ae97f8cbd9b1be32dd3f
Jon <krypt3dkn1gh7@gmail.com>
2017-02-11 03:22:45 -0500

    Update ExamDates.py

--- Calendar/ExamDates.py
+++ Calendar/ExamDates.py
@@ -39,13 +39,11 @@ print('[+] Gathering dates')
 resp = requests.get('https://www.offensive-security.com/exam.php', params=payload)
 
 soup = BeautifulSoup(resp.content, 'lxml')
-print('[+] The following dates are available:')
 for link in soup.findAll('a', href=True):
     if 'exam' in link['href']:
         date = re.search(r'\d{4}-\d{2}-\d{2}', link['href']).group()
         date = datetime.strptime(date, '%Y-%m-%d').strftime('%B %d, %Y')
         dateList += '{}\n'.format(date)
-        print('\t{}'.format(date))
 
 writingData('available.txt', dateList)
 desktopNotification(dateList)
